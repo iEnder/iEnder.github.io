cfda46d95d1c405d357bf6f9637e3f802ecf76bc
iEnder <pkmn2024@gmail.com>
2016-11-19 22:05:14 -0500

    Initial Webpage Commit

---
+++ script.js
@@ -0,0 +1,125 @@
+$(".typedHead").typed({
+  strings: ["Seth Church"],
+  typeSpeed: 70,
+  callback: function() {
+    $('.following').animate({
+      opacity: 1,
+      bottom: '-25px',
+    }, 1000);
+    $('.fa-arrow-circle-down').animate({
+      opacity: 1
+    });
+  },
+});
+$("#portfoliobtn, .fa-arrow-circle-down").click(function() {
+  $('html, body').animate({scrollTop: $("#portfolio").offset().top -10}, 1000);
+});
+$("#contactbtn").click(function() {
+  $('html, body').animate({scrollTop: $("footer").offset().top}, 1000);
+});
+
+particlesJS("particle s-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 1420.4657549380909
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+    },
+    "opacity": {
+      "value": 0.06313181133058181,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 5,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 11.83721462448409,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
