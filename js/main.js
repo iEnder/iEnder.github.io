b6725a943fb48933172053966012661bea62518d
Seth Church <pkmn2024@gmail.com>
2017-02-24 08:49:00 -0500

    fixed portfolio and form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 const menuButton = document.querySelector(".navigation__toggle");
+const collapsePortfolio = document.querySelector(".portfolio__collapse-btn");
+
+
 menuButton.addEventListener('click', (e) => {
-    document.querySelector("#navigationList").classList.toggle("menu-active");
+    document.querySelector(".pre-nav").classList.toggle("pre-nav--active");
+});
+
+collapsePortfolio.addEventListener('click', () => {
+    document.querySelector(".portfolio__content").classList.toggle("portfolio__content--active");
+    collapsePortfolio.classList.toggle("portfolio__collapse-btn--active");
 });
