b27918dfe753e878a5b814458ec4dae6943293c7
iEnder <pkmn2024@gmail.com>
2017-08-13 23:03:50 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const concat = require('gulp-concat');
+
+gulp.task('rebuild', ['sass'], function() {
+    browserSync.reload();
+})
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('sass', function() {
+    return gulp.src('_scss/*.scss')
+        .pipe(concat('main.scss'))
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(minify())
+        .pipe(browserSync.reload({ stream: true }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', 'js/*.js', '_scss/*.scss'], ['rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
